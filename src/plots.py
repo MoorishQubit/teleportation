78d34cc79832a4f8800c423b776c21c2bef2d081
MoorishQubit <zakaria.mzaouali@gmail.com>
2023-11-06 14:35:43 +0100

    5th commit

--- src/plots.py
+++ src/plots.py
@@ -14,21 +14,22 @@ plt.tick_params(axis='both', which='major', labelsize=20)
 
 b=[]
 if __name__ == '__main__':
-    pool = multiprocessing.Pool(20)
+    pool = multiprocessing.Pool(32)
     J=1.0
     mu=0.0
-    u=np.linspace(0,20,100)
-    for x in [8,10]:
+    u=0
+    w=np.linspace(0,0.99,100)
+    for x in [4,6,8,10]:
         func = partial(teleportation,x,J,mu,0)
-        b=pool.map(func,u)
-        np.savez("/home/zakaria/QT_Fermi-Hubbard/data/fig1b_L=%s"%x,b)
-        plt.plot(u,b,label=r'$L=%s$'%x)
+        b=pool.map(func,w)
+        np.savez("/home/zmzaouali/teleportation/data/fig3/fig3b_L=%s"%x,b)
+        plt.plot(w,b,label=r'$L=%s$'%x)
     plt.ylabel(r'$\mathcal{F}(1,L)$',fontsize=20)
-    plt.xlabel(r'$U$',fontsize=20)
+    plt.xlabel(r'$\delta$',fontsize=20)
     plt.legend(fontsize=20)
     plt.tick_params(axis='both', which='major', labelsize=18)
     plt.tight_layout()
-    plt.savefig('figures/fig1b.pdf')
+    plt.savefig('/home/zmzaouali/teleportation/figures/fig3b.pdf')
 
 # data=np.load("/home/zakaria/QT_Fermi-Hubbard/data/fig2c.npz")['arr_0']
 # w=np.linspace(0,0.99,50)
--- src/teleportation.py
+++ src/teleportation.py
@@ -5,9 +5,10 @@ def u(n,m):
     op=sum(np.exp(2*np.pi*1j*k*n/d)*(fock(d,k)*fock(d,(k+m)%d).dag()) for k in range(d))
     return op
 
-def teleportation(L,J,mu,w,U):
+def teleportation(L,J,mu,U,w):
     d=4
-    psi=sum(tensor(fock(d,r),fock(d,r)) for r in range(0,d))/np.sqrt(d) # maximaly entangled state \psi^+
+    #psi=sum(tensor(fock(d,r),fock(d,r)) for r in range(0,d))/np.sqrt(d) # maximaly entangled state \psi^+
+    psi=(tensor(basis(4,0), basis(4,3))+tensor(basis(4,3), basis(4,0))+tensor(basis(4,1), basis(4,2))+tensor(basis(4,2), basis(4,1)))/2 #hubbard state
     r_in=ket2dm(sum(fock(d,r) for r in range(d)).unit()) # the state to teleporte (density matrix of 1 ququart)
     state=sum(sum(((tensor(u(n,m),identity(d))*psi*psi.dag()*tensor(u(n,m),identity(d)).dag()*fermi_hubbard(L,J,U,mu,w)).tr())*u(n,-m)*r_in*u(n,-m).dag()
                   for n in range(d)) for m in range(d))
